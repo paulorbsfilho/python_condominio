c71e32b2148b72cc6bcc16bd4bc7508391f76145
paulorbsfilho <p4ulinho@outlook.com>
2018-05-30 22:40:44 -0300

    Atualizacao de view

--- core/views.py
+++ core/views.py
@@ -1,14 +1,10 @@
 # Create your views here.
 
 
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-from django.utils import timezone
 
 
-from .models import Apartamento, Proprietario
+from .models import Apartamento
 
 
 def index(request):
